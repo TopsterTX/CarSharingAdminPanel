38d390e15c96d71dd89076177594c6c7ec92201c
TopsterTX <bbs.100@yandex.ru>
2021-08-10 09:07:34 +0300

    V-09 начало работы

--- src/components/ContentRouter/ContentRouter.jsx
+++ src/components/ContentRouter/ContentRouter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import { Orders } from "./../Orders/Orders";
+import { Entities } from "./../Entities/Entities";
 
 export const ContentRouter = () => {
   return (
     <React.Fragment>
       <Route path="/admin/panel/main" />
-      <Route path="/admin/panel/main" />
+      <Route path="/admin/panel/entities" render={() => <Entities />} />
       <Route path="/admin/panel/orders" render={() => <Orders />} />
     </React.Fragment>
   );
---
+++ src/components/Entities/Entities.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Entities.scss";
+
+export const Entities = () => {
+  return (
+    <section className="entities">
+      <div className="entities__container">
+          <h2 className="entities__title"></h2>
+          <div className="entities__block">
+              
+          </div>
+      </div>
+    </section>
+  );
+};
